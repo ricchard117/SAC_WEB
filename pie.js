9de3a11b35cab8371a0bbb56490745c35dd4a830
ricchard117 <60483677+ricchard117@users.noreply.github.com>
2020-02-08 19:57:52 -0600

    Add files via upload

---
+++ pie.js
@@ -0,0 +1,50 @@
+/**
+ * ---------------------------------------
+ * This demo was created using amCharts 4.
+ * 
+ * For more information visit:
+ * https://www.amcharts.com/
+ * 
+ * Documentation is available at:
+ * https://www.amcharts.com/docs/v4/
+ * ---------------------------------------
+ */
+
+// Themes begin
+am4core.useTheme(am4themes_dark);
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+var chart = am4core.create("chartdiv", am4charts.PieChart3D);
+chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+
+chart.data = [
+  {
+    country: "PROV_004",
+    litres: 19
+  },
+  {
+    country: "PROV_002",
+    litres: 7
+  },
+   {
+    country: "PROV_003",
+    litres: 4
+  },
+   {
+    country: "PROV_001",
+    litres: 3
+  },
+];
+
+chart.innerRadius = am4core.percent(40);
+chart.depth = 120;
+
+chart.legend = new am4charts.Legend();
+
+var series = chart.series.push(new am4charts.PieSeries3D());
+series.dataFields.value = "litres";
+series.dataFields.depthValue = "litres";
+series.dataFields.category = "country";
+series.slices.template.cornerRadius = 5;
+series.colors.step = 6;
